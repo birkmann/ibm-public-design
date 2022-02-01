b6558115496f2f63e03af4d6966254dcf9246c9a
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-02-01 17:53:49 +0100

    files

--- 01/tailwind.config.js
+++ 01/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   // These paths are just examples, customize them to match your project structure
-  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     // ...
   },
