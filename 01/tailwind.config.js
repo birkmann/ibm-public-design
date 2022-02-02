5bf7af2c0ba1a0fcf37d05ef9ef51dede7ecf1fe
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-02-02 12:53:12 +0100

    design

--- 01/tailwind.config.js
+++ 01/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   // These paths are just examples, customize them to match your project structure
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
-    // ...
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', "sans-serif"],
+      },
+    },
   },
   // ...
 };
